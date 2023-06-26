0d577b531aa60ae25d6d9360e728c05667cccbe0
jiaruonan <jiaruonan97@gmail.com>
2023-06-26 19:17:04 +0800

    update(jrn): add another compile method for pybind

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # PyCppBridge
-Examples demonstrating bidirectional interaction between Python and C++ code.
 
+Examples show bidirectional interaction between Python and C++.
 
 ### cython
-```
+
+```Shell
 cd cython/
 bash compile.sh
 python demo.py
 ```
 
 ### pybind
-```
+
+```Shell
 cd pybind/
 git clone https://github.com/pybind/pybind11.git
 bash compile.sh
 python main.py
+```
+
+Another Compile Method: (Reference: https://www.jianshu.com/p/0b4b49dd706a)
+
+```Shell
+cd pybind/
+git clone https://github.com/pybind/pybind11.git
+cd another_compile/
+c++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o example$(python3-config --extension-suffix)
+python run_example.py
 ```
---
+++ pybind/another_compile/example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <pybind11/stl.h>
+#include <pybind11/pybind11.h>
+
+std::vector<std::vector<double>> matrix_multiply(const std::vector<std::vector<double>>& A, const std::vector<std::vector<double>>& B) {
+    int n = A.size();
+    int m = B[0].size();
+    int p = A[0].size();
+
+    std::vector<std::vector<double>> result(n, std::vector<double>(m, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < p; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(example, m) {
+    m.def("matrix_multiply", &matrix_multiply, "Matrix multiplication");
+}
